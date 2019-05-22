afc07e8442e9b11cd41503252ab31afcb377755a
Imran Qasim <imran_qasim@hotmail.com>
2019-05-23 04:52:20 +0500

    Part 18 updates

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -1,16 +1,36 @@
-(function(blocks, element){
+(function(blocks, element, editor){
 	var el = element.createElement;
+	var RichTextEditor = editor.RichText;
+
 	blocks.registerBlockType('pwspk-plugin-dev/pwspk-block01', {
 		title: 'PWSPK Block',
 		icon: 'admin-home',
 		category: 'layout',
+		attributes:{
+			content:{
+				type: 'array',
+				source: 'children',
+				selector: 'div'
+			}
+		},
 		edit: function(prop){
-
-			return el('p', {className: prop.className}, "Hello I'm in Editor")
+			var content = prop.attributes.content;
+			function editText(newText){
+				prop.setAttributes({content: newText});
+			}
+			return el(RichTextEditor,{
+				tagName: 'div',
+				onChange: editText,
+				className: prop.className,
+				value: prop.attributes.content
+			});
 		},
 		save: function(prop){
-			return el('p', {className: prop.className}, "Hello I'm in Frontend")
+			return el(RichTextEditor.Content, {
+				tagName: 'div',
+				value: prop.attributes.content,
+			});
 		}
 	});
 
-}(window.wp.blocks, window.wp.element));
+}(window.wp.blocks, window.wp.element, window.wp.editor));
