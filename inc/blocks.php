f92ce2c900454f7f3089e5ab6acc832975b732ac
Imran Qasim <imran_qasim@hotmail.com>
2019-05-22 22:52:28 +0500

    part 17 updates

--- inc/blocks.php
+++ inc/blocks.php
@@ -4,9 +4,15 @@ class pwspkBlocks{
 		add_action('init', array($this, 'pwspk_custom_block_01'));
 	}
 	public function pwspk_custom_block_01(){
-		wp_register_script('pwspk-blocks-script', PLUGIN_URL."/assets/js/blocks.js", array('wp-blocks','wp-element'));
+		wp_register_script('pwspk-blocks-script', PLUGIN_URL."assets/js/blocks.js", array('wp-blocks','wp-element'));
+		wp_register_style('pwspk-admin-style', PLUGIN_URL."assets/css/editor.css");
+		wp_register_style('pwspk-front-style', PLUGIN_URL."assets/css/blocks.css");
 		$name = "pwspk-plugin-dev/pwspk-block01";
-		$args = array('editor_script' => 'pwspk-blocks-script');
+		$args = array(
+			'style' => 'pwspk-front-style',
+			'editor_style' => 'pwspk-admin-style',
+			'editor_script' => 'pwspk-blocks-script'
+		);
 		register_block_type($name, $args );
 	}
 } new pwspkBlocks;
