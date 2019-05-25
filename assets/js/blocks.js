2b315433db07a72cf8ea32457a474c07220ac3e6
Imran Qasim <imran_qasim@hotmail.com>
2019-05-25 10:10:55 +0500

    Part 19 updates

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -1,9 +1,14 @@
-(function(blocks, element, editor){
+(function(blocks, element, editor, components){
 	var el = element.createElement;
 	var RichTextEditor = editor.RichText;
+	var AlignmentToolbar = editor.AlignmentToolbar;
+	var BlockControls = editor.BlockControls;
+	var InspectorControls = editor.InspectorControls;
+	var PanelBody = components.PanelBody;
 
 	blocks.registerBlockType('pwspk-plugin-dev/pwspk-block01', {
 		title: 'PWSPK Block',
+		description: 'Test Block for YouTube Tutorial on Perfect Web Solutions',
 		icon: 'admin-home',
 		category: 'layout',
 		attributes:{
@@ -11,26 +16,63 @@
 				type: 'array',
 				source: 'children',
 				selector: 'div'
+			},
+			alignment:{
+				type: 'string',
+				default: 'none',
 			}
 		},
 		edit: function(prop){
 			var content = prop.attributes.content;
+			var alignment = prop.attributes.alignment;
 			function editText(newText){
 				prop.setAttributes({content: newText});
 			}
-			return el(RichTextEditor,{
-				tagName: 'div',
-				onChange: editText,
-				className: prop.className,
-				value: prop.attributes.content
-			});
+			function changeAlignment(newAlignment){
+				prop.setAttributes({alignment: newAlignment === undefined ? 'none' : newAlignment});
+			}
+			return [
+				el(BlockControls, {
+					key: 'controls',
+				},el(AlignmentToolbar, {
+					value: alignment,
+					onChange: changeAlignment,
+				}
+				)
+				),
+
+
+				el(InspectorControls, {
+					key: 'controls',
+				},el(PanelBody, {
+					title: 'Alignment Control',
+				},
+				el(AlignmentToolbar, {
+					value: alignment,
+					onChange: changeAlignment,
+				}
+				),
+				),
+				),
+	
+				el(RichTextEditor,{
+					tagName: 'div',
+					onChange: editText,
+					style: {textAlign: alignment},
+					className: prop.className,
+					value: content
+				}
+			)
+
+			]; 
 		},
 		save: function(prop){
 			return el(RichTextEditor.Content, {
 				tagName: 'div',
+				style: {textAlign: prop.attributes.alignment },
 				value: prop.attributes.content,
 			});
 		}
 	});
 
-}(window.wp.blocks, window.wp.element, window.wp.editor));
+}(window.wp.blocks, window.wp.element, window.wp.editor, window.wp.components));
--- inc/blocks.php
+++ inc/blocks.php
@@ -4,7 +4,7 @@ class pwspkBlocks{
 		add_action('init', array($this, 'pwspk_custom_block_01'));
 	}
 	public function pwspk_custom_block_01(){
-		wp_register_script('pwspk-blocks-script', PLUGIN_URL."assets/js/blocks.js", array('wp-blocks','wp-element','wp-editor'));
+		wp_register_script('pwspk-blocks-script', PLUGIN_URL."assets/js/blocks.js", array('wp-blocks','wp-element','wp-editor', 'wp-components'));
 		wp_register_style('pwspk-admin-style', PLUGIN_URL."assets/css/editor.css");
 		wp_register_style('pwspk-front-style', PLUGIN_URL."assets/css/blocks.css");
 		$name = "pwspk-plugin-dev/pwspk-block01";
